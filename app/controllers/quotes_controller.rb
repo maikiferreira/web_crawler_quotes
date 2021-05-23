68171b1eef71334126e5814b45196819964b0600
Maiki Neves Ferreira <maikiferreira38@gmail.com>
2021-05-23 13:34:07 -0300

    Configurando as variaveis

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,48 +3,42 @@ require 'open-uri'
 
 class QuotesController < ApplicationController
 
+  # Constante para armazenar o valor do token necessário para se ter acesso a API.
   TOKEN = "secret123"
+  URL = 'http://quotes.toscrape.com/'
 
   include ActionController::HttpAuthentication::Token::ControllerMethods
   
   before_action :authenticate
   before_action :set_quote, only: [:index]
 
-
-
   # Inicia a pesquisa de frases com a tag passada como parametro na url.
   # Se a tag existir no documento de tags será feito uma pesquisa no documento
   # de Quote para obter as frases que possuem a tag pesquisada. Se a tag não
   # existir no documento de Tag será feito um crawler para obter as frases com
   # a tag passada como parâmetro
-  def iniciar
-    pesquisa = pesquisarTagBanco(params[:quote])
-
+  def start
+    result = searchTagBank(params[:quote])
     # Se a tag não for encontrada no banco será feito o crawler no site.
-    if(pesquisa.length == 0)
+    if(result.length == 0)
       crawlerSite(params[:quote])
     end
-
-    pesquisa = pesquisarQuoteBanco(params[:quote])
-    render json: pesquisa
-
+    result = searchQuoteBank(params[:quote])
+    render json: result
   end
 
-
-
-
   # Pesquisa no documento de Tag a tag passada como parâmetro.
-  def pesquisarTagBanco(param)
+  def searchTagBank(param)
     Tag.where(tag: param)
   end
 
   # Pesquisa no documento de Quote as quotes que possuem a tag passada como parâmetro.
-  def pesquisarQuoteBanco(param)
+  def searchQuoteBank(param)
     Quote.where(tags: { '$in': [param]})
   end
 
   # Adiciona uma Tag no documento de Tags se ela não estiver presente no documento.
-  def adicionarTag(param)
+  def addTag(param)
     if(Tag.where(tag: param).length == 0)
       Tag.create!(
         tag: param
@@ -53,7 +47,7 @@ class QuotesController < ApplicationController
   end
 
   # Adiciona uma Quote no documento de Quotes se ela não estiver presente no documento.
-  def adicionarQuote(quot, autho, author_abou, ta)
+  def addQuote(quot, autho, author_abou, ta)
     frase = Quote.where(quote: quot)
       if (frase.length == 0)
         Quote.create!(
@@ -67,41 +61,36 @@ class QuotesController < ApplicationController
 
   # Obtém as tags de uma quote do conteudo do site onde estão presentes as tags
   # Retorna uma array onde cada posição é uma tag da quote.
-  def obterTagsQuote(tags, param)
+  def obtainTagsQuote(tags, param)
     tag = []
     tags.search('a.tag').each do |a|
       tag.push(a.text)
       if(a.text == param)
-        adicionarTag(param)
+        addTag(param)
       end
       next if a['class'] == 'tag'
     end
     tag
   end
 
-
-
   # Executa o crawler para obter as informações do site e armazenar no banco de dados.
   def crawlerSite(param)
-    url = 'http://quotes.toscrape.com/'
-    html = URI.open(url)
+    html = URI.open(URL)
     doc = Nokogiri::HTML(html)
+    fields = doc.css('div.col-md-8')
 
-    campos = doc.css('div.col-md-8')
-
-    campos.search('div.quote').each do |div|
+    fields.search('div.quote').each do |div|
             quote = div.css('span.text').text
             author = div.css('small.author').text
             author_about = "http://quotes.toscrape.com/" + div.css('a')[0].attributes["href"].value  
 
-            tag = obterTagsQuote(div.css('div.tags'), param)
+            tag = obtainTagsQuote(div.css('div.tags'), param)
             
             if (tag.include?(param))
-              adicionarQuote(quote, author, author_about, tag)
+              addQuote(quote, author, author_about, tag)
             end
         next if div['class'] == 'tags'
     end
-
   end
 
   # Extrai o token da requisição e compara se é igual a constante TOKEN
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/quotes/:quote', to: 'quotes#iniciar'
+  get '/quotes/:quote', to: 'quotes#start'
 end
