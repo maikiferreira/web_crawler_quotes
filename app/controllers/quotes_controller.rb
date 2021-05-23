97d65b8e45addf68d25119ea2d28fdfb967a49bd
Maiki Neves Ferreira <maikiferreira38@gmail.com>
2021-05-23 10:49:29 -0300

    Criaçao das funçoes para pesquisa nos documentos de Quota e Tag do mongodb

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,18 @@ class QuotesController < ApplicationController
   #before_action :set_quote, only: [:show, :update, :destroy]
   before_action :set_quote, only: [:index]
   
-  def showw
-    render json: params[:quote]
+  def iniciar
+    #render json: params[:quote]
+    render json: pesquisarTagBanco(params[:quote])
   end
+end
+
+# Pesquisa no documento de Tag a tag passada como parâmetro.
+def pesquisarTagBanco(param)
+  Tag.where(tag: param)
+end
+
+# Pesquisa no documento de Quote as quotes que possuem a tag passada como parâmetro.
+def pesquisarQuoteBanco(param)
+  Quote.where(tags: { '$in': [param]})
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   #resources :tags
   #resources :quotes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/quotes/:quote', to: 'quotes#showw'
+  get '/quotes/:quote', to: 'quotes#iniciar'
 end
